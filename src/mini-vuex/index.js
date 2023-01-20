31ec8cc12841cc38a914e5a300be08fdb711ddb6
tangyouhua <tangyouhua@126.com>
2023-01-20 11:29:47 +0800

    实现 commit 和 dispatch

--- src/mini-vuex/index.js
+++ src/mini-vuex/index.js
@@ -12,6 +12,8 @@ export function createStore(options) {
         set state(v) {
             console.error('please use replaceState() to reset state')
         },
+        _mutations: options.mutations,
+        _actions: options.actions,
     }
     // Object.defineProperty(store, 'state', {
     //     get state() {
@@ -21,6 +23,31 @@ export function createStore(options) {
     //         console.error('please use replaceState() to reset state')
     //     },
     // })
+    // commit(type, payload)
+    function commit(type, payload) {
+        // 获取type对应的mutation
+        const entry = this._mutations[type]
+        if (!entry) {
+            console.error(`unknown mutation type: ${type}`)
+            return
+        }
+        entry.call(this.state, this.state, payload)
+    }
+
+    // dispatch(type, payload)
+    function dispatch(type, payload) {
+        // 获取用户编写的type对应的action
+        const entry = this._actions[type]
+        if (!entry) {
+            console.error(`unknown action type: ${type}`)
+            return
+        }
+        return entry.call(this, this, payload)
+    }
+
+    store.commit = commit.bind(store) // commit调用时的上下文为store
+    store.dispatch = commit.bind(store) // dispatch调用时的上下文为store
+
     // 插件实现要求的install方法
     store.install = function (app) {
         const store = this
